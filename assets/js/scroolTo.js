ba8e00814397e5ca3198ef3c26d962cf43939563
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-07-18 21:53:21 +0500

    booker-jasur

---
+++ assets/js/scroolTo.js
@@ -0,0 +1,26 @@
+document.getElementById('scrollBtn').addEventListener('click', function () {
+    scrollTo(100, 600); // 100px ga 600ms davomida tushish
+});
+
+function scrollTo(targetPosition, duration) {
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        const timeElapsed = currentTime - startTime;
+        const run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
